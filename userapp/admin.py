d6a476ef8fba930d730bcd94d2e31a5898886635
NiShadrin <nishadrin@gmail.com>
2022-01-06 16:24:46 +0300

    add hash password in admins panel

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from userapp.models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+
+    # list_display = ('email', 'title', 'user_type')
+    # list_filter = ('user_type',)
+    exclude = ('groups', 'user_permissions')
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            orig_obj = User.objects.get(pk=obj.pk)
+            if obj.password != orig_obj.password:
+                obj.set_password(obj.password)
+            obj.save()
+        else:
+            obj.set_password(obj.password)
+            super().save_model(request, obj, form, change)
+
+admin.site.register(User, UserAdmin)
