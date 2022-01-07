2c4f3ea94b068d5b8b63d4256848d670196dea94
andrew <ceddoy@mail.ru>
2022-01-07 15:37:53 +0700

    добавил тесты для проверки rules анонимом

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,24 @@
+from http import HTTPStatus
+
+from django.contrib import auth
+from django.test.client import Client
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestMainPageForAnon(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_main_page_rules(self):
+        response = self.client.get('/rules/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_anonymous)
+
+
+
