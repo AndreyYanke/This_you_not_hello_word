4c3a84b1b2159336d9481eea19820ae6cfd03814
andrew <ceddoy@mail.ru>
2022-01-07 16:48:08 +0700

    Заменил функции на классы, MainPageView наследовался от TemplateView - это неокончательный класс

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, FormView
+
+from userapp.models import User
 
 
-def main_page(request):
-    return render(request, 'mainapp/index.html')
+class MainPageView(TemplateView):
+    """Класс, который полностью разворачивает главную страницу"""
+    template_name = 'mainapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['list_company_partners'] = User.objects.filter(is_partner=True)
+        return context
 
 
-def rules_site(request):
-    return render(request, 'mainapp/rules.html')
+class RulesSiteView(TemplateView):
+    """Класс, который переносит на страницу с полной редакцией правил портала"""
+    template_name = 'mainapp/rules.html'
