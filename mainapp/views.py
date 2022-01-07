a3b568281fc3036aac2719a357e8bc46ec411ba6
andrew <ceddoy@mail.ru>
2022-01-07 17:34:17 +0700

    Добавил в MainPageView context list_news для отображения новостей

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, FormView
 
+from newsapp.models import NewsPost
 from userapp.models import User
 
 
@@ -12,6 +13,7 @@ class MainPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['list_company_partners'] = User.objects.filter(is_partner=True)
+        context['list_news'] = NewsPost.objects.all().order_by('-created_at')
         return context
 
 
