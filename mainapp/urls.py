7a023c76a351e781a22de7b3942102741cfb466c
andrew <ceddoy@mail.ru>
2022-01-07 16:46:44 +0700

    Заменил в urls функции на классы

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,6 +5,6 @@ import mainapp.views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', mainapp.main_page, name='main'),
-    path('rules/', mainapp.rules_site, name='rules')
+    path('', mainapp.MainPageView.as_view(), name='main'),
+    path('rules/', mainapp.RulesSiteView.as_view(), name='rules')
 ]
