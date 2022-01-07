77d0a061c29b4525e3fb056ccd8f4d0430921ab7
andrew <ceddoy@mail.ru>
2022-01-07 15:09:32 +0700

    добавил view rules_site (правила портала)

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'mainapp/index.html')
+
+
+def rules_site(request):
+    return render(request, 'mainapp/rules.html')
