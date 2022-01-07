7ed1c4f94ab4c1f61af421337755400c5471615b
andrew <ceddoy@mail.ru>
2022-01-07 23:29:17 +0700

    поменял родителя в MainPageView, также добавил форму авторизации

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,24 @@
-from django.views.generic import TemplateView
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, FormView
 
 from newsapp.models import NewsPost
+from this_you_not_hello_word.settings import LOGIN_REDIRECT_URL
 from userapp.models import User
 
 
-class MainPageView(TemplateView):
+class MainPageView(FormView):
     """Класс, который полностью разворачивает главную страницу"""
+    form_class = AuthenticationForm
     template_name = 'mainapp/index.html'
+    success_url = LOGIN_REDIRECT_URL
+
+    """В случае, если POST (авторизация)"""
+    def form_valid(self, form):
+        auth.login(self.request, form.get_user())
+        return HttpResponseRedirect(self.get_success_url())
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -14,6 +26,10 @@ class MainPageView(TemplateView):
         context['list_news'] = NewsPost.objects.filter(is_active=True).order_by('-created_at')
         context['user'] = self.request.user
         return context
+
+
+class UserLogoutView(LogoutView):
+    template_name = 'mainapp/index.html'
 
 
 class RulesSiteView(TemplateView):
