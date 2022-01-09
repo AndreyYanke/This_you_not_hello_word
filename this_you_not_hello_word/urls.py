a61a13e839b345971a48745d351d62c4d9fe1728
andrew <ceddoy@mail.ru>
2022-01-09 18:44:20 +0700

    добавил include regist

--- this_you_not_hello_word/urls.py
+++ this_you_not_hello_word/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='main')),
     path('news/', include('newsapp.urls', namespace='news')),
+    path('regist/', include('userapp.urls', namespace='regist')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
