79d9bdb7f39e960fbd5145cf483503663610eb62
andrew <ceddoy@mail.ru>
2022-01-10 02:36:16 +0700

    создал миксин для авторизации после регистрации

---
+++ userapp/mixins.py
@@ -0,0 +1,15 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect
+
+from this_you_not_hello_word.settings import LOGIN_REDIRECT_URL
+
+
+class AuthAfterRegistMixin:
+    success_url = LOGIN_REDIRECT_URL
+
+    def form_valid(self, form):
+        self.object = form.save()
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+        auth.login(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
