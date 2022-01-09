e883fd6ff59f506974d6d195e48b1b00714533f9
andrew <ceddoy@mail.ru>
2022-01-09 16:27:05 +0700

    добавил удобное отображение в админ-панели

--- userapp/admin.py
+++ userapp/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from userapp.models import User, City
 
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user_type')
+    list_filter = ('user_type',)
+    exclude = ('groups', 'user_permissions')
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(City)
