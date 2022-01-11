d19f10de5b747c7e8c7cc658d452e79270d5a1fa
andrew <ceddoy@mail.ru>
2022-01-12 05:37:44 +0700

    Можно было все не переписывать, просто exclude

--- resumeapp/forms.py
+++ resumeapp/forms.py
@@ -6,10 +6,11 @@ from resumeapp.models import Resume
 class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
-        fields = (
-            'first_name', 'last_name', 'photo', 'sex', 'age', 'citizenship', 'city', 'contact_info', 'ready_to_move', 'position', 'salary',
-            'work_schedule','busyness','work_experiences','education', 'about_myself', 'is_publish'
-        )
+        # fields = (
+        #     'first_name', 'last_name', 'photo', 'sex', 'age', 'citizenship', 'city', 'contact_info', 'ready_to_move', 'position', 'salary',
+        #     'work_schedule','busyness','work_experiences','education', 'about_myself', 'is_publish'
+        # )
+        exclude = ('user',)
 
 
     def __init__(self, *args, **kwargs):
