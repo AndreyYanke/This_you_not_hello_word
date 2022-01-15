ddb2d0618433146e80750ca4357949f8aef09ecd
geekbrains <ayankeav@gmail.com>
2022-01-15 22:26:29 +0300

    in resumeapp.views add CRUD

--- resumeapp/views.py
+++ resumeapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 
 from resumeapp.forms import ResumeForm
@@ -10,23 +11,24 @@ from this_you_not_hello_word.settings import LOGIN_REDIRECT_URL
 class CreateResumeView(CreateView):
     """Создание резюме для соискателя"""
     model = Resume
-    template_name = 'resumeapp/resume_page.html'
-    success_url = '/'
+    template_name = 'resumeapp/resume_create.html'
     form_class = ResumeForm
+    success_url = reverse_lazy('main:main')
 
 
 class UpdateResumeView(UpdateView):
     """Обновление резюме для соискателя"""
     model = Resume
-    template_name = 'resumeapp/resume_page.html'
-    success_url = '/'
+    template_name = 'resumeapp/resume_update.html'
     form_class = ResumeForm
+    success_url = reverse_lazy('main:main')
 
 
 class DeleteResumeView(DeleteView):
     """Удаление резюме для соискателя"""
     model = Resume
-    template_name = 'mainapp/index.html'
+    template_name = 'resumeapp/resume_update.html'
+    success_url = reverse_lazy('main:main')
 
 
 class ResumeDetailView(LoginRequiredMixin, DetailView):
@@ -36,7 +38,6 @@ class ResumeDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'resume'
     login_url = LOGIN_REDIRECT_URL
 
-
     def get_context_data(self, **kwargs):
         resume = self.get_object()
         context = super().get_context_data(**kwargs)
@@ -45,12 +46,9 @@ class ResumeDetailView(LoginRequiredMixin, DetailView):
         context['educations'] = resume.education.select_related()
 
         context['sex'] = [status[1] for status in config.STATUS_SEX if status[0] == resume.sex][0]
-        context['busyness'] = [status[1] for status in config.STATUS_CHOICES_BUSYNESS if status[0] == resume.busyness][0]
+        context['busyness'] = [status[1] for status in config.STATUS_CHOICES_BUSYNESS if status[0] == resume.busyness][
+            0]
         context['work_schedule'] = [status[1] for status in config.STATUS_CHOICES_WORK_SCHEDULE if
                                     status[0] == resume.work_schedule][0]
 
         return context
-
-
-
-
