744b620522a74d3edf1ba63e42cb181c05b6daca
andrew <ceddoy@mail.ru>
2022-01-15 18:39:30 +0700

    поменял success_url в UserLoginView

--- userapp/views.py
+++ userapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LogoutView, LoginView
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
 
 from this_you_not_hello_word.settings import LOGIN_REDIRECT_URL
@@ -14,17 +15,21 @@ class FirstPageRegistration(TemplateView):
 
 
 class CompanyRegistration(AuthAfterRegistMixin, CreateView):
+
     """Регистрация для компании"""
     template_name = 'userapp/regist_company.html'
     form_class = CompanyRegisterForm
     initial = {'user_type': User.USER_TYPE_COMPANY}
+    success_url = LOGIN_REDIRECT_URL
 
 
 class AspirantRegistration(AuthAfterRegistMixin, CreateView):
+
     """Регистрация для соискателя"""
     template_name = 'userapp/regist_aspirant.html'
     form_class = AspirantRegisterForm
     initial = {'user_type': User.USER_TYPE_USER}
+    success_url = reverse_lazy('vacancy:list')
 
 
 class UserLogoutView(LogoutView):
@@ -33,5 +38,14 @@ class UserLogoutView(LogoutView):
 
 class UserLoginView(LoginView):
     template_name = 'userapp/authorisation.html'
-    success_url = LOGIN_REDIRECT_URL
     form_class = AuthenticationForm
+
+    def get_success_url(self):
+        if self.request.user.user_type == User.USER_TYPE_USER:
+            url = reverse_lazy('vacancy:list')
+        elif self.request.user.user_type == User.USER_TYPE_COMPANY:
+            # Добавить list view резюме вместо LOGIN_REDIRECT_URL
+            url = LOGIN_REDIRECT_URL
+        else:
+            url = LOGIN_REDIRECT_URL
+        return url
