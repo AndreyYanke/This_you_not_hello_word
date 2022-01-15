6eb4f9952ed2e6af19f92b64e893322e7eb4492a
andrew <ceddoy@mail.ru>
2022-01-15 18:51:42 +0700

    Добавил хеширование для админ панели

--- userapp/admin.py
+++ userapp/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from userapp.models import User, City
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserViewAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'user_type')
     list_filter = ('user_type',)
-    exclude = ('groups', 'user_permissions')
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            orig_obj = User.objects.get(pk=obj.pk)
+            if obj.password != orig_obj.password:
+                obj.set_password(obj.password)
+            obj.save()
+        else:
+            obj.set_password(obj.password)
+            super().save_model(request, obj, form, change)
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserViewAdmin)
 admin.site.register(City)
