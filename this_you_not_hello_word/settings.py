461953737ce5d3cd5fef4d8f23fb77cb22b72fb3
andrew <ceddoy@mail.ru>
2022-01-17 07:42:55 +0700

    Подключил в apps django-filters

--- this_you_not_hello_word/settings.py
+++ this_you_not_hello_word/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_filters',
     'django_extensions',
     'userapp.apps.UserappConfig',
     'vacancyapp.apps.VacancyappConfig',
