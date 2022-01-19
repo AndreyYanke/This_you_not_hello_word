0dda0ca2edf76193511de9436dfe8b904c227f24
andrew <ceddoy@mail.ru>
2022-01-19 20:06:43 +0700

    fix result site for view

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,20 +5,20 @@ from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 from newsapp.models import NewsPost
-from this_you_not_hello_word.settings import LOGIN_REDIRECT_URL
 from userapp.models import User
 
 
 class MainPageView(TemplateView):
     """Класс, который полностью разворачивает главную страницу"""
-    # form_class = AuthenticationForm
     template_name = 'mainapp/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['list_company_partners'] = User.objects.filter(partner=True)
         context['list_news'] = NewsPost.objects.filter(is_active=True).values('pk', 'title').order_by('-created_at')[:5]
+        context['form'] = AuthenticationForm()
         return context
+
 
 class UserLogoutView(LogoutView):
     template_name = 'mainapp/index.html'
