a7291f2453f1df7ce3a8f2cc858d8509e4f13e00
Nikolos123 <Isponec2>
2022-02-06 19:14:43 +0300

    update project

--- userapp/forms.py
+++ userapp/forms.py
@@ -41,6 +41,8 @@ class AspirantUpdateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     def clean_phone_number(self):
         data = self.cleaned_data['phone_number']
